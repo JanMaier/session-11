eea78a7709f18b30e7d43c3d46b17197f1aff593
JanMaier <janhendrik.maier@sciencespo.fr>
2017-11-24 19:52:22 +0100

    essais

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var vm = new Vue({
   data: function () {
     var now = new Date();
     var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-   	
+   	var person = ['Merkel', 'Seehofer', 'Lindner', 'Oezdemir', 'Steinmeier', 'Schulz']
+
     return  {
     	person: ['Merkel', 'Seehofer', 'Lindner', 'Oezdemir', 'Steinmeier', 'Schulz']
     };
